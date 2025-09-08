4973b70ab54b7c066f427d30410a2c2973084d54
MaiconMW <maicon.w312@outlook.com>
2025-09-08 20:40:20 -0300

    Camada de dados concluida.

---
+++ Pokedex/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Pokedex.Models;
+
+namespace Pokedex.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+        }
+
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<PokemonTipo> PokemonTipos { get; set; }
+        public DbSet<Regiao> Regioes { get; set; }
+        public DbSet<Tipo> Tipos { get; set; }
+    }
+}
+
+//Classe responsavel por controle de acesso.
